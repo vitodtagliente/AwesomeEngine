8cd553600873708a19b592e5f242fca9589ad742
Vito Domenico Tagliente <hackjoker@hotmail.it>
2018-03-12 21:44:52 +0100

    sprite animation component

--- engine/include/awesome/gameplay/components/sprite_animation_component.h
+++ engine/include/awesome/gameplay/components/sprite_animation_component.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <map>
+#include <string>
+#include <vector>
 #include "../../scenegraph/component.h"
+#include "../../rendering/components/sprite_rendering_component.h"
 #include "../../math/rect.h"
+#include "../../rendering/shading/texture.h"
 
 namespace Awesome
 {
@@ -9,17 +14,34 @@ namespace Awesome
 	{
 		struct SpriteAnimationData
 		{
-
+			Rendering::Texture* texture{ nullptr };
+			Math::Rect frame_rect{ 0.0f,0.0f,1.0f,1.0f };
+			float frame_time{ 0.5f };
 		};
 
 		struct SpriteAnimation
 		{
-
+			std::vector<SpriteAnimationData> frames;
 		};
 
 		class SpriteAnimationComponent : public Scenegraph::Component
 		{
+		private:
+			float time{ 0.0f };
+			int frame_index{ 0 };
+			bool in_pause{ false };
+			bool playing{ false };
+			Rendering::SpriteRenderingComponent* sprite_rendering_component{ nullptr };
+
 		public:
+			std::map<std::string, SpriteAnimation> animations;
+			SpriteAnimation* animation{ nullptr };
+
+			void play();
+			void play(std::string animation_name);
+			void pause();
+			void stop();
+
 			void init() override;
 			void update(float delta_time) override;
 		};
