680705f4a4ceb39cc09a4b55354cae8947888aee
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2019-04-30 23:50:40 +0200

    application refactoring

--- engine/src/awesome/application/application.cpp
+++ engine/src/awesome/application/application.cpp
@@ -1,16 +1,22 @@
 #include "application.h"
 
+#include "time.h"
 #include "window.h"
 
 namespace awesome
 {
 	Application::Application()
-		: m_window(nullptr)
+		: m_input(nullptr),
+		m_time(nullptr),
+		m_window(nullptr)
 	{
 	}
 
 	Application::~Application()
 	{
+		delete m_input;
+		delete m_window;
+		delete m_time;
 	}
 
 	bool Application::startup()
@@ -20,6 +26,11 @@ namespace awesome
 
 	void Application::shutdown()
 	{
+	}
+
+	void Application::tick(const double t_deltaTime)
+	{
+
 	}
 
 	Window* Application::getWindow() const
--- engine/src/awesome/application/application.h
+++ engine/src/awesome/application/application.h
@@ -4,6 +4,8 @@
 
 namespace awesome
 {
+	class Input;
+	class Time;
 	class Window;
 
 	class Application : public Module<Application>
@@ -16,11 +18,17 @@ namespace awesome
 		virtual bool startup() override;
 		virtual void shutdown() override;
 
+		virtual void tick(const double t_deltaTime = 0.0) override;
+
 		// get the main window
 		Window* getWindow() const;
 
 	protected:
 
+		// application's input
+		Input* m_input;
+		// application's time
+		Time* m_time;
 		// application's window
 		Window* m_window;
 
