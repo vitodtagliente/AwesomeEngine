ba7e924391f7cb65b187e099d4db6142dfeb7047
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2019-05-28 22:57:29 +0200

    changes

--- engine/src/awesome/graphics/mesh/mesh.cpp
+++ engine/src/awesome/graphics/mesh/mesh.cpp
@@ -2,6 +2,72 @@
 
 namespace awesome
 {
+	Mesh::Mesh()
+		: m_topology(Topology::Triangles)
+		, m_positions()
+		, m_uv()
+		, m_normals()
+		, m_indices()
+	{
+	
+	}
+
+	Mesh::Mesh(const Topology t_topology)
+		: m_topology(t_topology)
+		, m_positions()
+		, m_uv()
+		, m_normals()
+		, m_indices()
+	{
+	}
+
+	Mesh::Mesh(const Topology t_topology, 
+		const std::vector<vector3>& t_positions, 
+		const std::vector<unsigned int>& t_indices)
+		: m_topology(t_topology)
+		, m_positions(t_positions)
+		, m_uv()
+		, m_normals()
+		, m_indices(t_indices)
+	{
+	}
+
+	Mesh::Mesh(const Topology t_topology, 
+		const std::vector<vector3>& t_positions,
+		const std::vector<vector2>& t_uv, 
+		const std::vector<unsigned int>& t_indices)
+		: m_topology(t_topology)
+		, m_positions(t_positions)
+		, m_uv(t_uv)
+		, m_normals()
+		, m_indices(t_indices)
+	{
+	}
+
+	Mesh::~Mesh()
+	{
+	}
+
+	void Mesh::setPositions(const std::vector<vector3>& t_positions)
+	{
+		m_positions = t_positions;
+	}
+
+	void Mesh::setUV(const std::vector<vector2>& t_uv)
+	{
+		m_uv = t_uv;
+	}
+
+	void Mesh::setNormals(const std::vector<vector3>& t_normals)
+	{
+		m_normals = t_normals;
+	}
+
+	void Mesh::setIndices(const std::vector<unsigned int>& t_indices)
+	{
+		m_indices = t_indices;
+	}
+
 	Mesh * const Mesh::create(const Primitive t_primitive)
 	{
 		return nullptr;
--- engine/src/awesome/graphics/mesh/mesh.h
+++ engine/src/awesome/graphics/mesh/mesh.h
@@ -26,12 +26,26 @@ namespace awesome
 		};
 
 		Mesh();
+		Mesh(const Topology t_topology);
+		Mesh(const Topology t_topology, 
+			const std::vector<vector3>& t_positions, 
+			const std::vector<unsigned int>& t_indices);
+		Mesh(const Topology t_topology,
+			const std::vector<vector3>& t_positions,
+			const std::vector<vector2>& t_uv, 
+			const std::vector<unsigned int>& t_indices);
 		virtual ~Mesh();
 
 		inline Topology getTopology() const { return m_topology; }
 		inline const std::vector<vector3>& getPositions() const { return m_positions; }
 		inline const std::vector<vector2>& getUV() const { return m_uv; }
 		inline const std::vector<vector3>& getNormals() const { return m_normals; }
+		inline const std::vector<unsigned int>& getIndices() const { return m_indices; }
+
+		void setPositions(const std::vector<vector3>& t_positions);
+		void setUV(const std::vector<vector2>& t_uv);
+		void setNormals(const std::vector<vector3>& t_normals);
+		void setIndices(const std::vector<unsigned int>& t_indices);
 
 		static Mesh * const create(const Primitive t_primitive);
 
