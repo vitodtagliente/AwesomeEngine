e3d061693721fd1c4b32b777c78870d9cfe28e65
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2019-06-15 10:35:19 +0200

    changes

--- engine/src/awesome/editor/views/object_viewer.cpp
+++ engine/src/awesome/editor/views/object_viewer.cpp
@@ -23,6 +23,7 @@ namespace awesome
 		if (selection != nullptr)
 		{
 			ImGui::Begin("Object Viewer");
+			ImGui::Text(selection->name.data().c_str());
 			ImGui::InputFloat3("Position", &selection->transform.position.data[0], 2);
 			ImGui::InputFloat3("Rotation", &selection->transform.rotation.data[0], 2);
 			ImGui::InputFloat3("Scale", &selection->transform.scale.data[0], 2);
