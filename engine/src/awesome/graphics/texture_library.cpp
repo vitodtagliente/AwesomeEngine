d8bfeb063c15627a9f485f223208d07f1de7d3c8
Vito Domenico Tagliente <hackjoker@hotmail.it>
2019-06-17 09:24:34 +0200

    changes

--- engine/src/awesome/graphics/texture_library.cpp
+++ engine/src/awesome/graphics/texture_library.cpp
@@ -3,4 +3,35 @@
 namespace awesome
 {
 
+	TextureLibrary::TextureLibrary()
+		: m_textures()
+	{
+
+	}
+
+	TextureLibrary::~TextureLibrary()
+	{
+
+	}
+
+	void TextureLibrary::clear()
+	{
+
+	}
+
+	unsigned int TextureLibrary::loadTextures(const std::string& t_path)
+	{
+		return 0;
+	}
+
+	Texture* const TextureLibrary::getTexture(const std::string& t_name) const
+	{
+		const auto it = m_textures.find(t_name);
+		if (it != m_textures.end())
+		{
+			return it->second;
+		}
+		return nullptr;
+	}
+
 }
--- engine/src/awesome/graphics/texture_library.h
+++ engine/src/awesome/graphics/texture_library.h
@@ -1,11 +1,30 @@
 #pragma once
 
+#include <map>
+#include <string>
+
 namespace awesome
 {
 	class Texture;
 
 	class TextureLibrary
 	{
+	public:
+
+		TextureLibrary();
+		~TextureLibrary();
+
+		void clear();
+
+		// load textures form path
+		unsigned int loadTextures(const std::string& t_path);
+
+		Texture* const getTexture(const std::string& t_name) const;
+
+	private:
+
+		// cached textures
+		std::map<std::string, Texture*> m_textures;
 
 	};
 }
