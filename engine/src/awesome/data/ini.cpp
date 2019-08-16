ab68af90adda4677fe1f4bb205ad396fbb8be82e
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2019-08-16 08:54:30 +0200

    adding ini file reader

--- engine/src/awesome/data/ini.cpp
+++ engine/src/awesome/data/ini.cpp
@@ -7,6 +7,7 @@ namespace awesome
 {
 	FileINI FileINI::Reader::parse(const std::string& t_filename)
 	{
+		/*
 		FileINI file;
 		std::string currentSection;
 		for (const std::string& line : File::readLines(t_filename))
@@ -25,16 +26,65 @@ namespace awesome
 			}
 		}
 		return file;
+		*/
+		return {};
 	}
 
 	FileINI::FileINI()
-		: m_data()
+		: m_sections()
 	{
 
 	}
 	
 	bool FileINI::hasSection(const std::string& t_name) const
 	{
-		return m_data.find(t_name) != m_data.end();
+		return m_sections.find(t_name) != m_sections.end();
+	}
+
+	FileINI::Section& FileINI::getSection(const std::string& t_name)
+	{
+		return m_sections[t_name];
+	}
+	
+	bool FileINI::Section::contains(const std::string& t_key) const
+	{
+		return m_data.find(t_key) != m_data.end();
+	}
+	
+	std::string FileINI::Section::get(const std::string& t_key) const
+	{
+		auto it = m_data.find(t_key);
+		if (it != m_data.end())
+		{
+			return it->second;
+		}
+		return std::string();
+	}
+	
+	void FileINI::Section::set(const std::string& t_key, const std::string& t_value)
+	{
+		auto it = m_data.find(t_key);
+		if (it != m_data.end())
+		{
+			it->second = t_value;
+		}
+	}
+	
+	void FileINI::Section::remove(const std::string& t_key)
+	{
+		m_data.erase(t_key);
+	}
+	
+	std::string FileINI::Section::to_string() const
+	{
+		std::string result;
+
+		result.append("[").append(m_name).append("]\n");
+		for (const auto& pair : m_data)
+		{
+			result.append(pair.first).append("=").append(pair.second).append("\n");
+		}
+
+		return result;
 	}
 }
--- engine/src/awesome/data/ini.h
+++ engine/src/awesome/data/ini.h
@@ -5,9 +5,45 @@
 
 namespace awesome
 {
-	class FileINI
+	class FileINI final
 	{
 	public:
+
+		class Section final
+		{
+		public:
+
+			Section()
+				: m_name()
+				, m_data()
+			{}
+
+			Section(const std::string& t_name)
+				: m_name(t_name)
+				, m_data()
+			{
+
+			}
+
+			// retrieve section name
+			inline const std::string& getName() const { return m_name; }
+			// retrieve section data
+			inline const std::map<std::string, std::string>& getData() const { return m_data; }
+
+			bool contains(const std::string& t_key) const;
+			std::string get(const std::string& t_key) const;
+			void set(const std::string& t_key, const std::string& t_value);
+			void remove(const std::string& t_key);
+
+			std::string to_string() const;
+
+		private:
+
+			// section name
+			std::string m_name;
+			// section data
+			std::map<std::string, std::string> m_data;
+		};
 
 		class Reader
 		{
@@ -19,9 +55,10 @@ namespace awesome
 		FileINI();
 
 		bool hasSection(const std::string& t_name) const;
+		Section& getSection(const std::string& t_name);
 
 	private:
 
-		std::map<std::string, std::map<std::string, std::string>> m_data;
+		std::map<std::string, Section> m_sections;
 	};
 }
