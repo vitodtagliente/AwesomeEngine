62f76846785da7be538b2531545d9983bdc3c94c
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2019-09-01 20:05:56 +0200

    changes

--- engine/src/awesome/core/string_builder.cpp
+++ engine/src/awesome/core/string_builder.cpp
@@ -2,5 +2,37 @@
 
 namespace awesome
 {
+	StringBuilder::StringBuilder()
+		: m_data()
+	{
+		
+	}
 
+	void StringBuilder::append(const std::string& t_str)
+	{
+		if (m_data.size() > 0)
+		{
+			m_data.back().append(t_str);
+		}
+		else
+		{
+			m_data.push_back(t_str);
+		}
+	}
+
+	void StringBuilder::appendLine(const std::string& t_str)
+	{
+		m_data.push_back(t_str);
+	}
+	
+	std::string StringBuilder::toString() const
+	{
+		std::string result;
+		for (const std::string& str : m_data)
+		{
+			result.append(str);
+			result.append("\n");
+		}
+		return result;
+	}
 }
--- engine/src/awesome/core/string_builder.h
+++ engine/src/awesome/core/string_builder.h
@@ -1,6 +1,23 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 namespace awesome
 {
+	class StringBuilder
+	{
+	public:
 
+		StringBuilder();
+
+		void append(const std::string& t_str);
+		void appendLine(const std::string& t_str);
+
+		std::string toString() const;
+
+	private:
+
+		std::vector<std::string> m_data;
+	};
 }
