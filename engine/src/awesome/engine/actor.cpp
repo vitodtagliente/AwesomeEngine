cd958c72c984575b1f6a39bca8ccd45576f2caf0
Vito Domenico Tagliente <hackjoker@hotmail.it>
2019-09-09 13:59:19 +0200

    changes

--- engine/src/awesome/engine/actor.cpp
+++ engine/src/awesome/engine/actor.cpp
@@ -1,6 +1,39 @@
 #include "actor.h"
 
+#include "component.h"
+
 namespace awesome
 {
+	void Actor::initialize()
+	{
+		initialize_implementation();
+	}
 
+	void Actor::update(const double t_deltaTime)
+	{
+		// update the transform if the object is not static
+		if (!isStatic)
+		{
+			transform.update();
+		}
+		// if active update all the components
+		if (active)
+		{
+			// update enabled components
+			// for (Component* const component : m_components)
+			// {
+			// 	if (component->enabled)
+			// 	{
+			// 		component->update(t_deltaTime);
+			// 	}
+			// }
+			// specific implementation
+			update_implementation(t_deltaTime);
+		}
+	}
+
+	void Actor::destroy()
+	{
+
+	}
 }
--- engine/src/awesome/engine/actor.h
+++ engine/src/awesome/engine/actor.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include <memory>
+#include <string>
+#include <vector>
 #include <awesome/core/string_id.h>
 #include <awesome/math/transform.h>
 
 namespace awesome
 {
+	class Component;
+
 	class Actor
 	{
 	public:
@@ -22,15 +26,38 @@ namespace awesome
 		// get object state
 		inline State getState() const { return m_state; }
 
+		// object lifetime
+		void initialize();
+		// object update 
+		void update(const double t_deltaTime);
+		// destroy the object
+		void destroy();
+
+		// object name
+		std::string name;
+		// transform
+		transform transform;
+		// if true, this object cannot be moved
+		bool isStatic;
+		// if true this object is active
+		bool active;
+
+	protected:
+
+		virtual void initialize_implementation() {}
+		virtual void update_implementation(const float /*t_deltaTime*/) {}
+
 	private:
 
 		// actor id
 		StringId m_id;
-		// actor transform
-		transform m_transform;
-		// parent actor
-		std::weak_ptr<Actor> m_parent;
 		// actor state
 		State m_state;
+		// parent actor
+		std::weak_ptr<Actor> m_parent;
+		// children actors
+		std::vector<std::weak_ptr<Actor>> m_children;
+		// actor components
+		std::vector<std::unique_ptr<Component>> m_components;
 	};
 }
