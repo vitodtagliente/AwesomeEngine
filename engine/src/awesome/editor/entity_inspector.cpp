547312b713279a709b7aa7446e75f74dde7b0d15
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2021-10-05 08:41:40 +0200

    Update entity_inspector.cpp

--- engine/src/awesome/editor/entity_inspector.cpp
+++ engine/src/awesome/editor/entity_inspector.cpp
@@ -13,6 +13,8 @@ void EntityInspector::render(Entity* const entity)
 	ImGui::Begin("Entity Inspector");
 	if (entity)
 	{
+		ImGui::InputText("name", const_cast<char*>(entity->name.c_str()), 100);
+		ImGui::InputText("tag", const_cast<char*>(entity->tag.c_str()), 100);
 		ImGui::InputFloat3("position", entity->transform.position.data);
 		ImGui::InputFloat3("rotation", entity->transform.rotation.data);
 		ImGui::InputFloat3("scale", entity->transform.scale.data);
