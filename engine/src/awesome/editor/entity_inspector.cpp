d67f0323c313965a3c7479e1d75f698194c22a02
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2021-10-09 11:33:05 +0200

    Update entity_inspector.cpp

--- engine/src/awesome/editor/entity_inspector.cpp
+++ engine/src/awesome/editor/entity_inspector.cpp
@@ -1,7 +1,10 @@
 #include "entity_inspector.h"
 
 #include <imgui.h>
+#include <awesome/scene/component.h>
 #include <awesome/scene/entity.h>
+#include <awesome/components/gizmos_renderer.h>
+#include <awesome/components/sprite_renderer.h>
 
 EntityInspector::EntityInspector()
 {
@@ -19,6 +22,24 @@ void EntityInspector::render(Entity* const entity)
 		ImGui::InputFloat3("rotation", entity->transform.rotation.data);
 		ImGui::InputFloat3("scale", entity->transform.scale.data);
 		ImGui::Checkbox("isStatic", &entity->transform.isStatic);
+
+		const auto& components = entity->getComponents();
+		for (auto it = components.begin(); it != components.end(); ++it)
+		{
+			Component* const component = *it;
+			if (SpriteRenderer* const renderer = dynamic_cast<SpriteRenderer*>(component))
+			{
+				ImGui::Separator();
+				ImGui::Text("SpriteRenderer");
+				ImGui::InputFloat4("rect", renderer->rect.data);
+			}
+			if (GizmosRenderer* const renderer = dynamic_cast<GizmosRenderer*>(component))
+			{
+				ImGui::Separator();
+				ImGui::Text("GizmosRenderer");
+				ImGui::ColorEdit4("background", renderer->color.data);
+			}
+		}
 	}
 	ImGui::End();
 }
