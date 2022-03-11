7a92018654548d7ce3176507c88e0bc8a02b2cf5
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2022-03-11 16:54:34 +0100

    changes

--- engine/src/awesome/data/asset.cpp
+++
@@ -1,7 +0,0 @@
-#include "asset.h"
-
-Asset::Asset(Type type)
-	: m_id()
-	, m_type(type)
-{
-}
--- engine/src/awesome/data/asset.h
+++ engine/src/awesome/data/asset.h
@@ -3,9 +3,12 @@
 
 #include <awesome/core/uuid.h>
 
+#include "image.h"
+
 class Asset
 {
 public:
+	friend class AssetLibrary;
 
 	enum class Type
 	{
@@ -13,23 +16,35 @@ public:
 		Prefab
 	};
 
-	Asset(Type type);
-
 	inline const uuid& getId() const { return m_id; }
 	inline Type getType() const { return m_type; }
+
+protected:
+	Asset(const Type type)
+		: m_id()
+		, m_type(type)
+	{
+
+	}
 
 private:
 	uuid m_id;
 	Type m_type;
 };
 
-template <typename T>
+template <Asset::Type T, typename D>
 class BaseAsset : public Asset
 {
 public:
-
+	D data;
 
 private:
-	
+	BaseAsset(const D& data)
+		: Asset(T)
+		, m_data(data)
+	{
 
+	}
 };
+
+typedef BaseAsset<Asset::Type::Image, Image> ImageAsset;
