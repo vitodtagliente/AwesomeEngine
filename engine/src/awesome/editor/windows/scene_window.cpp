84fbb5f011494c3c8c9fb6d875b8528332b5ad7e
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2022-04-16 16:57:28 +0200

    changes

--- engine/src/awesome/editor/windows/scene_window.cpp
+++ engine/src/awesome/editor/windows/scene_window.cpp
@@ -1,5 +1,8 @@
 #include "scene_window.h"
 
+#include <awesome/application/input.h>
+#include <awesome/core/string_util.h>
+#include <awesome/editor/icons.h>
 #include <awesome/editor/layout.h>
 #include <awesome/editor/state.h>
 #include <awesome/entity/entity.h>
@@ -7,62 +10,6 @@
 
 namespace editor
 {
-	SceneWindow::SceneWindow()
-		: m_list()
-	{
-		m_list.onAddItem = []() -> void
-		{
-			World& world = World::instance();
-			Entity* const newEntity = world.spawn();
-			newEntity->name = std::string("Entity ") + std::to_string(world.getEntities().size() + 1);
-			State::instance().select(newEntity);
-			Layout::scrollToBottom();
-		};
-
-		m_list.onItemSelection = [](const uuid& id) -> void
-		{
-			State::instance().select(World::instance().findEntityById(id));
-		};
-
-		m_list.onRemoveItem = [](const uuid& id) -> void
-		{
-			World& world = World::instance();
-			world.destroy(world.findEntityById(id));
-			const auto& entities = world.getEntities();
-			if (!entities.empty())
-			{
-				if (entities[0]->getId() == id)
-				{
-					if (entities.size() > 1)
-					{
-						State::instance().select(entities[1].get());
-					}
-					else
-					{
-						State::instance().select();
-					}
-				}
-				else
-				{
-					State::instance().select(entities[0].get());
-				}
-			}
-			else
-			{
-				State::instance().select();
-			}
-		};
-
-		m_list.onRenameItem = [](const uuid& id, const std::string& name) -> void
-		{
-			Entity* const entity = World::instance().findEntityById(id);
-			if (entity)
-			{
-				entity->name = name;
-			}
-		};
-	}
-
 	void SceneWindow::render()
 	{
 		State& state = State::instance();
@@ -71,12 +18,123 @@ namespace editor
 			? selection->asEntity()
 			: nullptr;
 
-		std::map<uuid, std::string> items;
+		const bool hasActiveFocus = Layout::isWindowFocused();
+		if (hasActiveFocus && selectedEntity != nullptr)
+		{
+			processInput(selectedEntity);
+		}
+
+		if (Layout::button(ICON_FA_PLUS))
+		{
+			addEntity();
+		}
+
+		Layout::sameLine();
+
+		const std::string previousFilter = m_filter;
+		Layout::input(ICON_FA_SEARCH, m_filter);
+		if (previousFilter != m_filter || !hasActiveFocus)
+		{
+			m_state = NavigationState::Navigating;
+		}
+
+		Layout::separator();
+
 		for (const auto& entity : World::instance().getEntities())
 		{
-			items.insert(std::make_pair(entity->getId(), entity->name));
+			const bool isSelected = selectedEntity != nullptr && entity->getId() == selectedEntity->getId();
+			if (isSelected && m_state == NavigationState::Renaming)
+			{
+				Layout::rename(m_tempRename);
+			}
+			else
+			{
+				if (!m_filter.empty() && !StringUtil::contains(entity->name, m_filter, StringUtil::CompareMode::IgnoreCase))
+				{
+					continue;
+				}
+
+				if (Layout::selectable(entity->name, isSelected))
+				{
+					m_state = NavigationState::Navigating;
+					m_tempRename = entity->name;
+					selectEntity(entity.get());
+				}
+			}
 		}
-		m_list.render(items, selectedEntity ? std::optional<uuid>(selectedEntity->getId()) : std::nullopt);
+	}
+
+	void SceneWindow::processInput(Entity* const entity)
+	{
+		Input& input = Input::instance();
+		if (m_state == NavigationState::Renaming)
+		{
+			if (input.isKeyPressed(KeyCode::Enter) || input.isKeyPressed(KeyCode::Escape))
+			{
+				m_state = NavigationState::Navigating;
+				renameEntity(entity, m_tempRename);
+			}
+		}
+		else
+		{
+			if (input.isKeyPressed(KeyCode::F2))
+			{
+				m_state = NavigationState::Renaming;
+			}
+			else if (input.isKeyPressed(KeyCode::Delete))
+			{
+				deleteEntity(entity);
+			}
+		}
+	}
+
+	void SceneWindow::addEntity()
+	{
+		World& world = World::instance();
+		Entity* const newEntity = world.spawn();
+		newEntity->name = std::string("Entity ") + std::to_string(world.getEntities().size() + 1);
+		State::instance().select(newEntity);
+		Layout::scrollToBottom();
+	}
+
+	void SceneWindow::deleteEntity(Entity* const entity)
+	{
+		const uuid id = entity->getId();
+		World& world = World::instance();
+		world.destroy(entity);
+		const auto& entities = world.getEntities();
+		if (!entities.empty())
+		{
+			if (entities[0]->getId() == id)
+			{
+				if (entities.size() > 1)
+				{
+					State::instance().select(entities[1].get());
+				}
+				else
+				{
+					State::instance().select();
+				}
+			}
+			else
+			{
+				State::instance().select(entities[0].get());
+			}
+		}
+		else
+		{
+			State::instance().select();
+		}
+	}
+
+	void SceneWindow::selectEntity(Entity* const entity)
+	{
+		State::instance().select(entity);
+	}
+
+	void SceneWindow::renameEntity(Entity* const entity, const std::string& name)
+	{
+		entity->name = name;
 	}
 
 	REFLECT_WINDOW(SceneWindow)
--- engine/src/awesome/editor/windows/scene_window.h
+++ engine/src/awesome/editor/windows/scene_window.h
@@ -4,15 +4,16 @@
 #include <string>
 
 #include <awesome/core/uuid.h>
-#include <awesome/editor/widgets/select_list.h>
 #include <awesome/editor/window.h>
+
+class Entity;
 
 namespace editor
 {
 	class SceneWindow : public Window
 	{
 	public:
-		SceneWindow();
+		SceneWindow() = default;
 
 		std::string getTitle() const override { return "Scene"; }
 		void render() override;
@@ -20,6 +21,21 @@ namespace editor
 		REFLECT()
 
 	private:
-		SelectList<uuid> m_list;
+
+		void processInput(Entity* const entity);
+		void addEntity();
+		void deleteEntity(Entity* const entity);
+		void selectEntity(Entity* const entity);
+		void renameEntity(Entity* const entity, const std::string& name);
+
+		enum class NavigationState
+		{
+			Navigating,
+			Renaming
+		};
+
+		std::string m_filter;
+		NavigationState m_state{ NavigationState::Navigating };
+		std::string m_tempRename;
 	};
 }
