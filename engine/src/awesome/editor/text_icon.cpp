6411655a35996595ed7a1b7ff6cefebd2b3e41f2
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2022-04-20 13:40:06 +0200

    changes

--- engine/src/awesome/editor/text_icon.cpp
+++ engine/src/awesome/editor/text_icon.cpp
@@ -2,6 +2,16 @@
 
 #include <IconsFontAwesome5.h>
 
+std::string editor::TextIcon::copy(const std::string& name)
+{
+    return std::string(ICON_FA_COPY) + name;
+}
+
+std::string editor::TextIcon::eraser(const std::string& name)
+{
+    return std::string(ICON_FA_ERASER) + name;
+}
+
 std::string editor::TextIcon::folder(const std::string& name)
 {
     return std::string(ICON_FA_FOLDER) + name;
@@ -10,6 +20,21 @@ std::string editor::TextIcon::folder(const std::string& name)
 std::string editor::TextIcon::minus(const std::string& name)
 {
     return std::string(ICON_FA_MINUS) + name;
+}
+
+std::string editor::TextIcon::paste(const std::string& name)
+{
+    return std::string(ICON_FA_PASTE) + name;
+}
+
+std::string editor::TextIcon::pause(const std::string& name)
+{
+    return std::string(ICON_FA_PAUSE) + name;
+}
+
+std::string editor::TextIcon::play(const std::string& name)
+{
+    return std::string(ICON_FA_PLAY) + name;
 }
 
 std::string editor::TextIcon::plus(const std::string& name)
@@ -27,7 +52,7 @@ std::string editor::TextIcon::search(const std::string& name)
     return std::string(ICON_FA_SEARCH) + name;
 }
 
-std::string editor::TextIcon::eraser(const std::string& name)
+std::string editor::TextIcon::stop(const std::string& name)
 {
-    return std::string(ICON_FA_ERASER) + name;
+    return std::string(ICON_FA_STOP) + name;
 }
--- engine/src/awesome/editor/text_icon.h
+++ engine/src/awesome/editor/text_icon.h
@@ -9,11 +9,16 @@ namespace editor
 	{
 		TextIcon() = delete;
 		
+		static std::string copy(const std::string& name = "");
 		static std::string eraser(const std::string& name = "");
 		static std::string folder(const std::string& name = "");
 		static std::string minus(const std::string& name = "");
+		static std::string paste(const std::string& name = "");
+		static std::string pause(const std::string& name = "");
+		static std::string play(const std::string& name = "");
 		static std::string plus(const std::string& name = "");
 		static std::string save(const std::string& name = "");
 		static std::string search(const std::string& name = "");
+		static std::string stop(const std::string& name = "");
 	};
 }
