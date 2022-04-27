a9fc103d4e99d1f83eb7762eb3d82587548773fc
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2022-04-27 10:09:55 +0200

    Update asset_filesystem.h

--- engine/src/awesome/data/asset_filesystem.h
+++ engine/src/awesome/data/asset_filesystem.h
@@ -13,5 +13,4 @@ struct AssetFilesystem final
 	static void move(const Asset::Descriptor& descriptor, const std::filesystem::path& path);
 	static bool remove(const Asset::Descriptor& descriptor);
 	static void rename(const Asset::Descriptor& descriptor, const std::string& name);
-
 };
