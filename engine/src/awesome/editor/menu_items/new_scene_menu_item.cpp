e903e445fb873c4609cc44d78f31810b12d6292c
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2022-04-29 23:41:27 +0200

    Update new_scene_menu_item.cpp

--- engine/src/awesome/editor/menu_items/new_scene_menu_item.cpp
+++ engine/src/awesome/editor/menu_items/new_scene_menu_item.cpp
@@ -1,11 +1,13 @@
 #include "new_scene_menu_item.h"
 
+#include <awesome/editor/state.h>
 #include <awesome/entity/world.h>
 
 namespace editor
 {
 	void NewSceneMenuItem::execute()
 	{
+		State::instance().select();
 		World::instance().clear();
 	}
 
