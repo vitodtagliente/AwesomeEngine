81a0800273ce3ca9ac036d2a14fb8fecd1eb3587
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2022-05-12 20:07:45 +0200

    Update game.cpp

--- sandbox/src/game.cpp
+++ sandbox/src/game.cpp
@@ -72,6 +72,16 @@ void Game::startup()
 	TypeFactory::load<Pawn>();
 	TypeFactory::load<PawnController>();
 	TypeFactory::load<Rotator>();
+
+	PrefabAssetPtr prefab = AssetLibrary::instance().find<PrefabAsset>(uuid("146999684233600"));
+	prefab->onLoad = [prefab]()
+	{
+		World& world = World::instance();
+		for (int i = 0; i < 0; ++i)
+		{
+			Entity* const entity = world.spawn(prefab, math::vec3(math::random(-10.f, 10.f), math::random(-10.f, 10.f), 0.0f));
+		}
+	};
 }
 
 void Game::shutdown()
