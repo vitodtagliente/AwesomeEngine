ce16fc659efcb4a3080af9d1994f4dfaaea7d4b3
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2022-10-31 22:15:11 +0100

    changes

--- sandbox/src/game.cpp
+++ sandbox/src/game.cpp
@@ -13,20 +13,7 @@
 
 void Game::startup()
 {
-	PrefabAssetPtr prefab = AssetLibrary::instance().find<PrefabAsset>(uuid("146999684233600"));
-	prefab->onLoad = [prefab]()
-	{
-		World& world = World::instance();
-		// for (int i = 0; i < 0; ++i)
-		// {
-		// 	world.spawn(prefab, math::vec3(math::random(-10.f, 10.f), math::random(-10.f, 10.f), 0.0f));
-		// }
-		Entity* const entity = world.spawn();
-		entity->name = "test";
-		Component* const component = TypeFactory::instantiate<Component>("HealthComponent");
-		entity->addComponent(component);
-		auto type = component->enabled;
-	};
+	
 }
 
 void Game::shutdown()
