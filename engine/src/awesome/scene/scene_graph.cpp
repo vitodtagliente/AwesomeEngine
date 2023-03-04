62e8c85cba0d35631c2b543da22875c1a054283f
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2023-03-04 16:05:47 +0100

    changes

--- engine/src/awesome/scene/scene_graph.cpp
+++ engine/src/awesome/scene/scene_graph.cpp
@@ -1,2 +1,37 @@
 #include "scene_graph.h"
 
+#include <thread>
+
+bool SceneGraph::load(const SceneAssetPtr& scene)
+{
+	if (m_state == State::Loading || scene->state != Asset::State::Ready)
+	{
+		return false;
+	}
+
+	m_scene = scene;
+	m_state = State::Loading;
+	std::thread handler([this]()
+		{
+			for (const Entity& entity : m_scene->data.entities)
+			{
+				m_root.addChild(std::make_unique<Entity>(entity));
+			}
+			m_state = State::Ready;
+		}
+	);
+	handler.detach();
+	return true;
+}
+
+bool SceneGraph::save(const std::filesystem::path& path)
+{
+	SceneAsset asset;
+	for (const auto& entity : m_root.children())
+	{
+		if (entity->transient) continue;
+
+		asset.data.entities.push_back(*entity);
+	}
+	return asset.save(path);
+}
--- engine/src/awesome/scene/scene_graph.h
+++ engine/src/awesome/scene/scene_graph.h
@@ -1,8 +1,10 @@
 /// Copyright (c) Vito Domenico Tagliente
 #pragma once
 
+#include <filesystem>
 #include <memory>
 
+#include <awesome/asset/scene_asset.h>
 #include <awesome/core/singleton.h>
 
 #include "entity.h"
@@ -10,10 +12,23 @@
 class SceneGraph final : public Singleton<SceneGraph>
 {
 public:
+	enum class State
+	{
+		Loading,
+		Ready
+	};
+
 	SceneGraph() = default;
 
 	Entity* const root() { return &m_root; }
 
+	bool load(const SceneAssetPtr& scene);
+	inline const SceneAssetPtr& scene() const { return m_scene; }
+	bool save(const std::filesystem::path& path);
+	inline State state() const { return m_state; }
+
 private:
 	Entity m_root;
+	SceneAssetPtr m_scene;
+	State m_state{ State::Ready };
 };
