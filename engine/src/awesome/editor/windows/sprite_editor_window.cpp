2d0e6fa2e6767c6fd1db428fea7d4077605c76ca
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2023-05-06 16:10:15 +0200

    changes

--- engine/src/awesome/editor/windows/sprite_editor_window.cpp
+++ engine/src/awesome/editor/windows/sprite_editor_window.cpp
@@ -1,6 +1,5 @@
 #include "sprite_editor_window.h"
 
-#include <awesome/asset/image_asset.h>
 #include <awesome/editor/editor_ui.h>
 
 char* const SpriteEditorWindow::getTitle() const
@@ -10,10 +9,32 @@ char* const SpriteEditorWindow::getTitle() const
 
 void SpriteEditorWindow::render()
 {
-	static ImageAsset s_image;
+	EditorUI::input("Image", m_image);
+	if (!m_image.ready()) return;
 
-	EditorUI::input("Image", s_image);
-	if (!s_image.ready()) return;
-
-
+	EditorUI::input("Columns", m_cols);
+	EditorUI::input("Rows", m_rows);
+	EditorUI::input("Column Index", m_columnIndex);
+	EditorUI::input("Row Index", m_rowIndex);
+	if (EditorUI::button((EditorUI::Icon::search + " Preview size").c_str()))
+	{
+		m_rect.width = static_cast<float>(1.0 / m_cols);
+		m_rect.height = static_cast<float>(1.0 / m_rows);
+	}
+	EditorUI::sameLine();
+	if (EditorUI::button((EditorUI::Icon::search + " Preview index").c_str()))
+	{
+		m_rect.x = static_cast<float>(m_columnIndex) * static_cast<float>(1.0 / m_cols);
+		m_rect.y = static_cast<float>(m_rowIndex) * static_cast<float>(1.0 / m_rows);
+	}
+	EditorUI::sameLine();
+	if (EditorUI::button((EditorUI::Icon::eraser + " Reset").c_str()))
+	{
+		m_rect.width = m_rect.height = 1.f;
+		m_cols = m_rows = 1;
+		m_rect.x = m_rect.y = 0.f;
+		m_columnIndex = m_rowIndex = 0;
+	}
+	EditorUI::input("Rect", m_rect);
+	EditorUI::image(m_image, m_rect);
 }
--- engine/src/awesome/editor/windows/sprite_editor_window.h
+++ engine/src/awesome/editor/windows/sprite_editor_window.h
@@ -1,11 +1,21 @@
 /// Copyright (c) Vito Domenico Tagliente
 #pragma once
 
+#include <awesome/asset/image_asset.h>
 #include <awesome/editor/window.h>
+#include <awesome/graphics/texture_rect.h>
 
 class SpriteEditorWindow : public Window
 {
 public:
 	virtual char* const getTitle() const override;
 	virtual void render() override;
+
+private:
+	int m_cols{ 1 };
+	int m_columnIndex{ 0 };
+	graphics::TextureRect m_rect; 
+	ImageAsset m_image;
+	int m_rowIndex{ 0 };
+	int m_rows{ 1 };
 };
