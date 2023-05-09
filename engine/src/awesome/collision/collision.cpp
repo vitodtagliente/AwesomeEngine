25a2bd27ae7fd703ccd710189b0caa0711351340
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2023-05-09 14:51:34 +0200

    Update collision.cpp

--- engine/src/awesome/collision/collision.cpp
+++ engine/src/awesome/collision/collision.cpp
@@ -1,6 +1,7 @@
 #include "collision.h"
 
 #include <awesome/components/collider2d_component.h>
+#include <awesome/graphics/graphics.h>
 #include <awesome/graphics/renderer.h>
 #include <awesome/scene/entity.h>
 
@@ -17,12 +18,17 @@ void Collision::shutdown()
 
 void Collision::render()
 {
+	Graphics* const gfx = Graphics::instance();
+	if (gfx == nullptr) return;
 
+	QuadTree::instance().render(*gfx->renderer);
 }
 
 void Collision::update(const double)
 {
 	QuadTree& qt = QuadTree::instance();
+	qt.clear();
+
 	for (Collider2dComponent* const collider : Collider2dComponent::components())
 	{
 		Entity* const entity = collider->getOwner();
